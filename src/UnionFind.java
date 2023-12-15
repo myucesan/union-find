6fbdef91a27c1991c1874acbb99e47e9a5396bb5
Mustafa <Ycesan>
2023-12-15 10:06:36 +0100

    added README.md

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,5 +1,47 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+class UnionFind {
+
+    private int[] parent;
+
+    private int[] rank;
+
+    // Union Find structure implemented with two arrays for Union by Rank
+    public UnionFind(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) parent[i] = i;
+    }
+
+    /**
+     * Merge two subtrees if they have a different parent, input is array indices
+     *
+     * @param i a node in the first subtree
+     * @param j a node in the second subtree
+     * @return true iff i and j had different parents.
+     */
+    boolean union(int i, int j) {
+        // TODO
+        return false; // change later
+    }
+
+    /**
+     * NB: this function should also do path compression
+     *
+     * @param i index of a node
+     * @return the root of the subtree containg i.
+     */
+    int find(int i) {
+        // TODO
+        return 1; // change later
+    }
+
+    // Return the rank of the trees
+    public int[] getRank() {
+        return rank;
+    }
+
+    // Return the parent of the trees
+    public int[] getParent() {
+        return parent;
     }
 }
--- src/UnionFindTest.java
+++ src/UnionFindTest.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class UnionFindTest {
+
+import java.util.Random;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class UnionFindTest {
+
+    @Test
+    public void example() {
+        UnionFind uf = new UnionFind(10);
+        Assertions.assertEquals(9, uf.find(9));
+        Assertions.assertTrue(uf.union(1, 2));
+        Assertions.assertTrue(uf.union(2, 3));
+        Assertions.assertTrue(uf.union(0, 1));
+        Assertions.assertTrue(uf.union(3, 4));
+        // Test that joining any combination will have no effect, as they are already joined
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++) Assertions.assertFalse(uf.union(i, j));
+        // Test whether all first five entries have the same root
+        for (int i = 0; i < 4; i++) Assertions.assertEquals(uf.find(i), uf.find(i + 1));
+        // Test whether all last five entries have themselves as root
+        for (int i = 5; i < 10; i++) Assertions.assertEquals(i, uf.find(i));
+    }
+
+    @Test
+    public void UnionTwiceInARow() {
+        UnionFind uf = new UnionFind(5);
+        Assertions.assertTrue(uf.union(1, 2));
+        Assertions.assertTrue(uf.union(1, 3));
+        Assertions.assertFalse(uf.union(2, 3));
+    }
+
+    @Test
+    public void rankNoUnionTest() {
+        UnionFind uf = new UnionFind(10);
+        // Test whether all entries have themselves as root
+        for (int i = 0; i < 10; i++) Assertions.assertEquals(i, uf.find(i));
+    }
+
+    @Test
+    public void findWithUnionTest() {
+        UnionFind uf = new UnionFind(10);
+        Assertions.assertTrue(uf.union(4, 2));
+        Assertions.assertFalse(uf.union(2, 4));
+        Assertions.assertEquals(uf.find(2), uf.find(4));
+    }
+
+    @Test
+    @Timeout(1)
+    public void fastEnough() {
+        int n = 1_000_000;
+        UnionFind uf = new UnionFind(n);
+        Random rng = new Random(1234);
+        for (int i = 0; i < n / 100; i++) {
+            int a = rng.nextInt(n);
+            int b = rng.nextInt(n);
+            uf.union(a, b);
+        }
+    }
 }
