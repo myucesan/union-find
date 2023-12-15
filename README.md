0bf219d5341baafed8b55a5a118274562b1dc7af
Mustafa <Ycesan>
2023-12-15 13:23:03 +0100

    added README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 The Union-Find data structure allows us to maintain disjoint sets. One way to implement the Union-Find data structure is to use Union-by-Rank. This data structure can be implemented by using two arrays (root and rank). The data structure for this implementation uses pointers. Each node has an associated pointer to the name of the set that contains this node. The Union-by-Rank data structure has been partly implemented. Implement the missing find and union methods.
 
 Note that the spec tests do not check whether you implemented the ranking correctly. They only check for the behaviour of the union and find methods, not their time!
+
+**This solution is complete.**
--- src/UnionFind.java
+++ src/UnionFind.java
@@ -20,19 +20,44 @@ class UnionFind {
      * @return true iff i and j had different parents.
      */
     boolean union(int i, int j) {
-        // TODO
-        return false; // change later
+        int parentI = find(i);
+        int parentJ = find(j);
+        if (parentI == parentJ) {
+            return false;
+        }
+
+        if (rank[parentI] > rank[parentJ]) {
+            parent[j] = parent[i];
+
+        } else if (rank[parentJ] > rank[parentI]) {
+            parent[i] = parent[j];
+        } else {
+            parent[parentJ] = parentI;
+            rank[parentJ]++;
+        }
+        return true; // change later
     }
 
     /**
      * NB: this function should also do path compression
      *
      * @param i index of a node
-     * @return the root of the subtree containg i.
+     * @return the root of the subtree containig i.
      */
     int find(int i) {
-        // TODO
-        return 1; // change later
+//        int result = i;
+//        while (rank[result] != 0) {
+//            result = parent[i];
+//        }
+        // thinking mistake above. Initially every node is the parent of itself, I was foolish comparing the rank.
+        // I have to check when a node is the parent of itself, then you know you have the root node and you want to attach
+        // other trees you union to that node
+
+        if (parent[i] != i) { // as long as i is not the parent of itself
+            parent[i] = find(parent[i]); // go up the tree until you reach the root (parent[i] == i)
+        }
+
+        return parent[i];
     }
 
     // Return the rank of the trees
